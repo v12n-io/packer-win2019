5fe87dffaa76b342d0ec767b4a121e7a9784f430
mpoore <2269147+mpoore@users.noreply.github.com>
2021-01-08 13:56:41 +0000

    Misc changes

---
+++ scripts/10-createuser.ps1
@@ -0,0 +1,15 @@
+# Create a standard user on the host
+# @author Michael Poore
+# @website https://blog.v12n.io
+# @source https://github.com/virtualhobbit
+$ErrorActionPreference = "Stop"
+
+# Variables
+$user = "REPLACEWITHWINDOWSUSER"
+$pass = "REPLACEWITHWINDOWSPASS"
+Add-Type -AssemblyName 'System.Web'
+
+# Create the user
+$secureString = ConvertTo-SecureString $pass -AsPlainText -Force
+New-LocalUser -Name $user -Password $secureString
+$credential = New-Object System.Management.Automation.PsCredential($ansibleUser,$secureString)
--- scripts/60-openssh.ps1
+++ scripts/60-openssh.ps1
@@ -7,12 +7,8 @@ $ErrorActionPreference = "Stop"
 # Install OpenSSH
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 
-# Set service to automatic and start
+# Set service to automatic
 Set-Service sshd -StartupType Automatic
-Start-Service sshd
 
 # Configure PowerShell as the default shell
 New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
-
-# Restart the service
-Restart-Service sshd
